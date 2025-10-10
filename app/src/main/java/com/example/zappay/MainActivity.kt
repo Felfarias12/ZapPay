7e9dbb9fa4e57abb4504c075bf3b0d640af305ce
unknown <Cetecom@SJAPCA03LC0408.duoc.acad>
2025-10-10 16:01:35 -0300

    agregue cosas en el build.gradle.kts para q funcionara la camara, como me dijo el profe pero no funciono o dijo q ahhi lo iba a ver

--- app/src/main/java/com/example/zappay/MainActivity.kt
+++ app/src/main/java/com/example/zappay/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.zappay.ui.CamaraFotos
 import com.example.zappay.ui.theme.ZapPayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ZapPayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CamaraFotos()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZapPayTheme {
-        Greeting("Android")
-    }
-}
