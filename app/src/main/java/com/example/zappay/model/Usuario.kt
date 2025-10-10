3101dd87409d187c475ea20d2b1c76ba2eb9829e
unknown <Cetecom@SJAPCA03LC0408.duoc.acad>
2025-10-10 15:15:33 -0300

    Agrego Usuario.kt y lleno la clase usuaroi, con nombre, apellido, entre otras cosas

--- app/src/main/java/com/example/zappay/model/Usuario.kt
+++ app/src/main/java/com/example/zappay/model/Usuario.kt
@@ -6,9 +6,24 @@ data class Usuario(
     var apellido: String,
     var nacionalidad: String,
     var telefono: String,
-    var correo: String
+    var correo: String,
+    var saldo: Double = 0.0,
+    var faceId: String = "" // pongo esto para simular la cara, aun nose como se coloca biometrico
 ) {
     fun mostrarInfo(): String {
-        return "ID: $id\nNombre: $nombre $apellido\nNacionalidad: $nacionalidad\nTeléfono: $telefono\nCorreo: $correo"
+        return "ID: $id\nNombre: $nombre $apellido\nNacionalidad: $nacionalidad\nTeléfono: $telefono\nCorreo: $correo\nSaldo: $$saldo"
+    }
+
+    fun realizarPago(monto: Double): Boolean {
+        return if (saldo >= monto) {
+            saldo -= monto
+            true
+        } else {
+            false
+        }
+    }
+
+    fun recargarSaldo(monto: Double) {
+        saldo += monto
     }
 }
