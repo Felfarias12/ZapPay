103eb2ddeb32fd4d9ee168b80dee94c5a7d5e3f2
Mario Jaramillo <marioymegamanzx8@gmail.com>
2025-12-13 23:49:06 -0300

    cambiando estetica en la pagina inicio + añadir el logo en esta

--- app/src/main/java/com/example/zappay/ui/PaginaInicio.kt
+++ app/src/main/java/com/example/zappay/ui/PaginaInicio.kt
@@ -1,23 +1,31 @@
 package com.example.zappay.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
+import com.example.zappay.R
+import androidx.compose.foundation.layout.size
 
 @Composable
 fun PaginaInicio(navController: NavController){
@@ -29,37 +37,58 @@ fun PaginaInicio(navController: NavController){
         verticalArrangement = Arrangement.Center
     ) {
 
-        Text(
-            text = "ZapPay",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.primary
-        )
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.logo_zappay),
+                contentDescription = "Logo ZapPay",
+                modifier = Modifier.size(60.dp).clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                text = "ZapPay",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         Text(
             text = "Paga con tu rostro",
-            style = MaterialTheme.typography.bodyLarge,
+            style = MaterialTheme.typography.titleMedium,
             textAlign = TextAlign.Center,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         Button(
             onClick = { navController.navigate("Login") },
-            modifier = Modifier.width(250.dp)
+            modifier = Modifier.fillMaxWidth().height(50.dp)
         ) {
-            Text(" Inicio Sesion")
+            Text(
+                text = " Inicio Sesion",
+                style = MaterialTheme.typography.titleMedium
+            )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(12.dp))
 
         Button(
             onClick = { navController.navigate("registroscreen") },
-            modifier = Modifier.width(250.dp)
+            modifier = Modifier.fillMaxWidth().height(50.dp)
         ) {
-            Text("Registrar Usuario")
+            Text(
+                text= "Registrar Usuario",
+                style = MaterialTheme.typography.titleMedium
+            )
         }
 
 
